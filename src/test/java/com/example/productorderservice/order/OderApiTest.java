1fc80e2ed6d3568bb24f28fd2c74fc37e163b99e
thisiswoo <thisiswoo0594@gmail.com>
2023-05-28 23:20:41 +0900

    14.API로 만든 상품 주문을 JPA로 적용하기

--- src/test/java/com/example/productorderservice/order/OderApiTest.java
+++ src/test/java/com/example/productorderservice/order/OderApiTest.java
@@ -1,15 +1,10 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.ApiTest;
-import com.example.productorderservice.product.ProductService;
 import com.example.productorderservice.product.ProductSteps;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,25 +15,13 @@ class OderApiTest extends ApiTest {
     void 상품주문() throws Exception {
         // given
         ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
-        final CreateOrderRequest request = 상품주문요청_생성();
+        final var request = OrderSteps.상품주문요청_생성();
 
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/orders")
-                .then()
-                .log().all().extract();
+        final var response = OrderSteps.상품주문요청(request);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    private static CreateOrderRequest 상품주문요청_생성() {
-        final Long productId = 1L;
-        final int quantity = 2;
-        return new CreateOrderRequest(productId, quantity);
     }
 
 }
