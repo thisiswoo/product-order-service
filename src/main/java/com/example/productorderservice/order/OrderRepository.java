f5574f1bc6e74754f86e8a79e7e9f6ac6e981cef
thisiswoo <thisiswoo0594@gmail.com>
2023-06-02 09:34:38 +0900

    18.주문결제 JPA 적용하기 - 사용자가 http(api)요청을 통해 주문결제 요청이 들어오면 결제 내용을 jpa를 사용하여 h2 DB에 저장하도록 구현하기

--- src/main/java/com/example/productorderservice/order/OrderRepository.java
+++ src/main/java/com/example/productorderservice/order/OrderRepository.java
@@ -2,5 +2,5 @@ package com.example.productorderservice.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- src/main/java/com/example/productorderservice/payment/PaymentAdapter.java
+++ src/main/java/com/example/productorderservice/payment/PaymentAdapter.java
@@ -1,6 +1,7 @@
 package com.example.productorderservice.payment;
 
 import com.example.productorderservice.order.Order;
+import com.example.productorderservice.order.OrderRepository;
 import com.example.productorderservice.product.DiscountPolicy;
 import com.example.productorderservice.product.Product;
 import org.springframework.stereotype.Component;
@@ -9,15 +10,18 @@ import org.springframework.stereotype.Component;
 class PaymentAdapter implements PaymentPort {
     private final PaymentGateway paymentGateway;
     private final PaymentRepository paymentRepository;
+    private final OrderRepository orderRepository;
 
-    PaymentAdapter(final PaymentGateway paymentGateway, final PaymentRepository paymentRepository) {
+    PaymentAdapter(final PaymentGateway paymentGateway, final PaymentRepository paymentRepository, final OrderRepository orderRepository) {
         this.paymentGateway = paymentGateway;
         this.paymentRepository = paymentRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public Order getOrder(final Long orderId) {
-        return new Order(new Product("상품1", 1000, DiscountPolicy.NONE), 2);
+
+        return orderRepository.findById(orderId).orElseThrow(() -> new IllegalArgumentException("Order not found"));
     }
 
     @Override
